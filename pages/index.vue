16bc14a30ad39af0ab595e3fd48a880272e0def9
Hui Beom Kim <khuibeom@gmail.com>
2022-10-21 12:11:28 +0900

    chore: fixed eslint indentation rule to be 4 spaces
    overriden vue/html-indent rule to force 4 space indentation

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    <h1>Index</h1>
-  </div>
+    <div>
+        <h1>Index</h1>
+    </div>
 </template>
