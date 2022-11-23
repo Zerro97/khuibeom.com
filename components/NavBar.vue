96a908637ae85e58f8a01c7aaa8402f8d9f0c57e
Hui Beom Kim <khuibeom@gmail.com>
2022-11-23 17:53:37 +0900

    feat(navbar): added logo

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -17,10 +17,8 @@ const tabs = ref([{
 <template>
   <div class="flex justify-between w-screen h-20 px-20 backdrop-blur-md bg-zinc-900">
     <div class="flex items-center justify-center h-full w-max">
-      <NuxtLink to="/">
-        <h1 class="mr-20 text-3xl text-zinc-300 hover:text-zinc-100">
-          hb_kim
-        </h1>
+      <NuxtLink to="/" class="p-2 mr-4 rounded hover:bg-zinc-800">
+        <img width="36" height="36" src="/logo/ladybug.png">
       </NuxtLink>
       <div class="flex items-center h-full gap-x-8">
         <NuxtLink v-for="tab in tabs" :key="tab.id" :to="tab.link">
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="min-h-screen bg-gradient-to-tr from-zinc-900 to-zinc-800 min-w-screen">
+  <div class="min-h-screen bg-zinc-900 min-w-screen">
     <NavBar />
     <slot />
   </div>
