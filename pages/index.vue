f524f1428550d4b682ccd59744c913a66fffa1a8
Hui Beom Kim <khuibeom@gmail.com>
2022-11-24 13:30:03 +0900

    feat: adding post and project section

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,51 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const posts = ref([{
+  title: 'lorem',
+  icon: 'logos:vitest',
+  date: '2022 Nov 22',
+  id: Math.random(),
+}, {
+  title: 'lorem',
+  icon: 'logos:vitest',
+  date: '2022 Nov 22',
+  id: Math.random(),
+}, {
+  title: 'lorem',
+  icon: 'logos:vitest',
+  date: '2022 Nov 22',
+  id: Math.random(),
+}, {
+  title: 'lorem',
+  icon: 'logos:vitest',
+  date: '2022 Nov 22',
+  id: Math.random(),
+}])
+
+const projects = ref([{
+  title: 'lorem',
+  description: 'testing contents',
+  icon: 'logos:vitest',
+  id: Math.random(),
+}, {
+  title: 'lorem',
+  description: 'testing contents',
+  icon: 'logos:vitest',
+  id: Math.random(),
+}, {
+  title: 'lorem',
+  description: 'testing contents',
+  icon: 'logos:vitest',
+  id: Math.random(),
+}, {
+  title: 'lorem',
+  description: 'testing contents',
+  icon: 'logos:vitest',
+  id: Math.random(),
+}])
+</script>
 
 <template>
-  <div class="flex flex-col">
+  <section class="flex flex-col w-full mt-20">
     <h1>
       Hi! I'm Hui Beom
     </h1>
@@ -21,5 +65,41 @@
         </NuxtLink>
       </button>
     </div>
-  </div>
+  </section>
+  <section class="flex flex-col w-full mt-20">
+    <h2>
+      Latest Posts
+    </h2>
+    <div class="flex flex-col w-full mt-6 gap-y-2">
+      <div v-for="post in posts" :key="post.id" class="flex items-center px-4 py-2 rounded group hover:bg-zinc-800 hover:cursor-pointer">
+        <Icon :name="post.icon" class="h-full mr-6 text-4xl" />
+        <div class="flex flex-col">
+          <h4 class="text-zinc-400 group-hover:text-zinc-100">
+            {{ post.title }}
+          </h4>
+          <p class="text-sm text-zinc-500 group-hover:text-zinc-400">
+            {{ post.date }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="flex flex-col w-full mt-20">
+    <h2>
+      Featured Projects
+    </h2>
+    <div class="grid w-full grid-cols-3 gap-2 mt-6">
+      <div v-for="project in projects" :key="project.id" class="flex px-4 py-2 rounded group hover:bg-zinc-800 hover:cursor-pointer">
+        <Icon :name="project.icon" class="h-full mr-6 text-4xl" />
+        <div class="flex flex-col">
+          <h4 class="text-zinc-400 group-hover:text-zinc-100">
+            {{ project.title }}
+          </h4>
+          <p class="text-sm text-zinc-500 group-hover:text-zinc-400">
+            {{ project.description }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
