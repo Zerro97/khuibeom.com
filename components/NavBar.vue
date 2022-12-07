ee2bb42d62a787a49b638687a63d885198958bcd
Hui Beom Kim <khuibeom@gmail.com>
2022-12-07 10:49:39 +0900

    feat: made navbar responsive

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -19,20 +19,20 @@ const tabs = ref([{
 </script>
 
 <template>
-  <div class="flex justify-between w-full h-20 backdrop-blur-md bg-zinc-900">
+  <div class="flex justify-between w-full h-16 md:h-20 backdrop-blur-md bg-zinc-900">
     <div class="flex items-center justify-center h-full w-max">
-      <NuxtLink to="/" class="p-2 mr-8 rounded hover:bg-zinc-800">
-        <nuxt-img width="34" height="34" class="min-w-[34px] min-h-[34px]" src="/logo/ladybug.png" />
+      <NuxtLink to="/" class="p-1 mr-3 rounded sm:p-2 sm:mr-4 md:mr-8 hover:bg-zinc-800">
+        <nuxt-img sizes="sm:100vw md:50vw lg:400px" width="34" height="34" src="/logo/ladybug.png" />
       </NuxtLink>
-      <div class="flex items-center h-full gap-x-8">
+      <div class="flex items-center h-full gap-x-4 sm:gap-x-6 md:gap-x-8">
         <NuxtLink v-for="tab in tabs" :key="tab.id" :to="tab.link">
-          <p class="text-lg text-zinc-300 hover:text-zinc-100">
+          <p class="text-base md:text-lg text-zinc-300 hover:text-zinc-100">
             {{ tab.label }}
           </p>
         </NuxtLink>
       </div>
     </div>
-    <div class="flex items-center justify-center h-full w-max gap-x-1">
+    <div class="items-center justify-center hidden h-full w-max gap-x-1 sm:flex">
       <NuxtLink to="https://github.com/Zerro97" class="p-1">
         <Icon name="mdi:github-face" class="text-3xl text-zinc-300 hover:text-zinc-100" />
       </NuxtLink>
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
 <template>
   <div class="flex flex-col min-h-screen bg-zinc-900 min-w-screen">
     <TopBar />
-    <div class="lg:w-[64rem] md:w-full px-10 mx-auto z-10 bg-zinc-900">
+    <div class="lg:w-[64rem] md:w-full px-6 md:px-10 mx-auto z-10 bg-zinc-900">
       <NavBar />
       <main class="mt-4">
         <slot />
