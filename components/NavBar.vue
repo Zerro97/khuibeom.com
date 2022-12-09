47ed14b991579d211e00be0d297ea21c7b9665fc
Hui Beom Kim <khuibeom@gmail.com>
2022-12-09 16:21:54 +0900

    feat: adjusted loading bar & navbar
    also removed skill card hovering effect

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -21,16 +21,16 @@ const tabs = ref([{
 <template>
   <div class="flex justify-between w-full h-16 md:h-20 backdrop-blur-md bg-zinc-900">
     <div class="flex items-center justify-center h-full w-max">
-      <NuxtLink to="/" class="p-1 mr-3 rounded sm:p-2 sm:mr-4 md:mr-8 hover:bg-zinc-800">
+      <NuxtLink to="/" class="p-1 mr-2 rounded sm:p-2 sm:mr-4 md:mr-8 hover:bg-zinc-800">
         <nuxt-img width="34" height="34" src="/logo/ladybug.png" />
       </NuxtLink>
-      <div class="flex items-center h-full gap-x-4 sm:gap-x-6 md:gap-x-8">
+      <div class="flex items-center h-full gap-x-3 sm:gap-x-5 md:gap-x-8">
         <NuxtLink
           v-for="tab in tabs"
           :key="tab.id"
           :to="tab.link"
-          class="text-base text-zinc-300 md:text-lg border-violet-500 hover:text-zinc-100"
-          active-class="border-b-3"
+          class="text-sm sm:text-base text-zinc-300 md:text-lg border-violet-500 hover:text-zinc-100"
+          active-class="!text-violet-400"
         >
           {{ tab.label }}
         </NuxtLink>
