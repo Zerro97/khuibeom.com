73358f6b7e6f8d4d07ecf6c03dcf75893c8e2267
Hui Beom Kim <khuibeom@gmail.com>
2022-12-10 20:29:47 +0900

    feat: made index page a little more mobile friendly

--- components/LinkButton.vue
+++ components/LinkButton.vue
@@ -6,7 +6,7 @@ defineProps({
 
 <template>
   <button class="px-4 py-2 border rounded bg-zinc-900 border-zinc-700 hover:bg-zinc-800">
-    <NuxtLink :to="to">
+    <NuxtLink :to="to" class="whitespace-nowrap">
       <slot />
     </NuxtLink>
   </button>
