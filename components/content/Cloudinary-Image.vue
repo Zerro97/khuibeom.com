5899c52c14fdebd8571d2c7c8e43a657bb82cb67
Hui Beom Kim <khuibeom@gmail.com>
2022-12-12 11:15:47 +0900

    feat: added a new doc page and minor style adjustment

---
+++ components/content/Cloudinary-Image.vue
@@ -0,0 +1,33 @@
+<script setup type="lang">
+const props = defineProps({
+  src: {
+    type: String,
+    default: '',
+  },
+  alt: {
+    type: String,
+    default: '',
+  },
+  width: {
+    type: [String, Number],
+    default: undefined,
+  },
+  height: {
+    type: [String, Number],
+    default: undefined,
+  },
+  figure: {
+    type: String,
+  },
+})
+</script>
+
+<template>
+  <figure v-if="figure">
+    <nuxt-img class="rounded" provider="cloudinary" :src="src" :alt="alt" :width="width" :height="height" />
+    <figcaption class="text-zinc-500">
+      {{ figure }}
+    </figcaption>
+  </figure>
+  <nuxt-img v-else class="rounded" provider="cloudinary" :src="src" :alt="alt" :width="width" :height="height" />
+</template>
--- components/content/ProseTr.vue
+++ components/content/ProseTr.vue
@@ -1,5 +1,5 @@
 <template>
-  <tr class="even:bg-zinc-800/50">
+  <tr class="even:bg-zinc-800/80">
     <slot />
   </tr>
 </template>
