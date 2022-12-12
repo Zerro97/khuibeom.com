c06bdf84ccdd1fe5e1787234cdedbba9cd6fb384
Hui Beom Kim <khuibeom@gmail.com>
2022-12-12 21:46:31 +0900

    feat: using debounce for changing copied icon

--- components/button/copy.vue
+++ components/button/copy.vue
@@ -3,8 +3,19 @@ const props = defineProps<{
   content: string
 }>()
 
+const isClicked = ref(false)
+
+const revertBackIcon = useDebounce(() => {
+  isClicked.value = false
+}, 1500)
+
+// Copy the contents of the code block to clipboard
 const copyToClip = () => {
   navigator.clipboard.writeText(props.content)
+
+  isClicked.value = true
+  // Every time copy button is clicked, transform the icon to checked icon
+  revertBackIcon()
 }
 </script>
 
@@ -14,6 +25,6 @@ const copyToClip = () => {
     aria-label="Copy Code"
     @click="copyToClip"
   >
-    <Icon name="mdi:content-copy" />
+    <Icon :name="`${isClicked ? 'mdi:check' : 'mdi:content-copy'}`" :class="`${isClicked && 'text-green-400'}`" />
   </button>
 </template>
